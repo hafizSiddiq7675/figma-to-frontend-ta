46d9736f0dbc2a0e1e1fe65a32b1267637c02732
mac <hafiz.siddiq.umer@gmail.com>
2022-08-15 17:57:24 +0500

    3rd cv template finalized

--- Figma-to-Laravel/CV/resources/views/cv/index.blade.php
+++ Figma-to-Laravel/CV/resources/views/cv/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
-    <link href="{{asset('assets/style.css')}}"    rel='stylesheet'>
+    <link href="{{asset('assets/style.css')}}" style="stylesheet" >
 
     <title>CV one</title>
 </head>
