fe608eecb2369217c6ad40e2f7827ca3e4ff74d9
mac <hafiz.siddiq.umer@gmail.com>
2022-11-29 15:48:36 +0500

    complete ten template

--- figma-laravel/resources/views/cv/index-ten-cv.blade.php
+++ figma-laravel/resources/views/cv/index-ten-cv.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
     <link rel="stylesheet" type="text/css" href="{{asset('assets/ten-cv.css')}}">
 
     <title>Ten CV</title>
@@ -25,27 +24,141 @@
                     <p class="name">SUR NAME</p> 
                     <p class="parah-text text-decoration-underline" >JOB POSITION</p> 
                     <p class="parah-text about">About Us</p> 
-                    <p class="parah-text at">''</p> 
-                    <p class="parah-text point text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore consequatur, numquam eum omnis a error. Dolores, 
-                    sit repudiandae! Iusto, nisi sint. Temporibus nisi aspernatur eos. Sapiente veritatis cumque repudiandae aliquid.</p>
+                    <p class="parah-text at">!</p> 
+                    <p class="parah-text point text-center">I am an experienced professional seeking an opportunity to my background in data analysis and market trend research. I am a dedicated and detail-oriented marketing spey professional skills. I am an experienced professional seeking an opportunity to my background in data 
+                            analysis and market trend research. I am a dedicated and detail-oriented marketing spey professional skills.</p>
                     </div>
                     
 
                 </div>
+                    <div>
+                       <button class="head-button">
+                        Contact 
+                       </button>
+                    </div>
+                    <div class="content-photo">
+                    <div class="content pictures">
+                        <img src="{{ asset('assets/images/email.png') }}" alt="profile picture" >                       
+                        <img src="{{ asset('assets/images/phone.png') }}" alt="profile picture" >                      
+                        <img src="{{ asset('assets/images/location.png') }}" alt="profile picture" >                 
+                       </div>
+                    <div class="content">
+                        <p>Email</p>
+                        <P>Phone Number</P>
+                        <P>Address</P>
+                    </div>
+                    </div>
+                    <div>
+                        <button class="head-button">
+                            Skills
+                        </button>
+                    </div>
+                    <div class="content">
+                        <p>Deveoping</p>
+                        <P>Programming</P>
+                        <P>Coding</P>
+                    </div>
             </div>
-         
+
          <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 seven-w">
         
             <div class="picture-frame">
                     <div class="d-flex justify-content-center">
                         <div class="btn btn-primary btn-rounded ">
-                            <label class="form-label  m-1" for="customFile1">Choose file</label>
-                            <input type="file" class="form-control d-none" id="customFile1" />
+                            <label class="form-label  m-1" >Choose file</label>
+                            <input type="file" class="form-control d-none"  />
                         </div>
                         
                     </div>
                    
-                </div>            
+                </div> 
+                <div>
+                <button class="head-button">
+                    Education
+                </button>
+                </div>
+
+                <div class="year-fit">
+                    <p>Bachelor Degree</p>
+                    <P class="year-gap">(years)</P>
+                </div>
+                    <p class="name-company">Name of institute</p>
+                    
+                    <div class="year-fit">
+                    <p>Master Degree</p>
+                    <p class="year-gap-one">(years)</p>
+                    </div>
+                    <p class="name-company">Name of institute</p>
+                <div>
+                <button class="head-button">
+                    Work Experience
+                </button>
+                </div>
+
+                <div class="year-fit">
+                    <p>Junior designer</p>
+                    <p class="year-gap-one">(years)</p>
+                </div>
+                    <p class="name-company">Name of company</p>
+                    
+                    <div class="year-fit">
+                    <p>Senior designer</p>
+                    <p class="year-gap-one">(years)</p>
+                    </div>
+                    <p class="name-company">Name of company</p>
+                    <div>
+                        <button class="head-button">
+                         Languages 
+                        </button>
+                     </div>
+                     <div class="content-language">
+                         <p>Urdu</p>
+                         <P>English</P>
+                         <P>Persian</P>
+                     </div>
+                
+                
+                
+                </div>
+              
+          
+                   
+                   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
          </div>
  
         </div>
@@ -58,6 +171,6 @@
    
     
      </div>
-   </div>
-     
+
+</body>  
 </html>
