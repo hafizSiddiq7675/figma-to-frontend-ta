29489fc55fbcbd8d8544ca6ba80deaef8d4bf0d1
mac <hafiz.siddiq.umer@gmail.com>
2022-12-15 15:14:19 +0500

    updated

--- figma-laravel/resources/views/text-generator/index.blade.php
+++ figma-laravel/resources/views/text-generator/index.blade.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assets/text-generator.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    {{--  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">  --}}
+    {{--  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">  --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
 
     <title>Document</title>
@@ -115,7 +115,7 @@
           <option value="Lucidatypewriter">Lucidatypewriter</option>
           <option value="Luminari">Luminari</option>
           <option value="Malgun Gothic">Impact</option>
-           <option value="Impact">Impact</option>
+           <option value="Fantasy">Fantasy</option>
           <option value="Impact">Impact</option>
           <option value="Impact">Impact</option>
           <option value="Impact">Impact</option>
